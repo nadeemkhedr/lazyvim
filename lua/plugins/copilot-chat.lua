91381f274312db32c90ddb8ca3d70d0f74ce4627
Nadeem Khedr <nadeemkhedr@gmail.com>
2025-07-17 17:01:17 +0300

    feat: update plugins

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -1,4 +1,12 @@
 return {
+  -- TODO: temp solution: https://github.com/LazyVim/LazyVim/issues/5899#issuecomment-2781398031
+  {
+    "zbirenbaum/copilot.lua",
+    optional = true,
+    opts = function()
+      require("copilot.api").status = require("copilot.status")
+    end,
+  },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     opts = {
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,6 +3,9 @@ return {
     "nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
+      servers = {
+        harper_ls = {},
+      },
     },
   },
 }
